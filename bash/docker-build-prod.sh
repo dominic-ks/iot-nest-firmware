452c659f37ccc81950e4cee2f14d0fff30637e1f
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2022-10-26 21:27:26 +0100

    updated build command

--- bash/docker-build-prod.sh
+++ bash/docker-build-prod.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker build -f Dockerfile.App -t europe-west2-docker.pkg.dev/iot-projects-309015/iot-nest-firmware/bdvs/iot-nest-firmware:latest .
-docker push europe-west2-docker.pkg.dev/iot-projects-309015/iot-nest-firmware/bdvs/iot-nest-firmware:latest
+docker buildx build -f Dockerfile.App  --platform linux/amd64,linux/arm64 -t europe-west2-docker.pkg.dev/iot-projects-309015/iot-nest-firmware/bdvs/iot-nest-firmware:latest --push .
