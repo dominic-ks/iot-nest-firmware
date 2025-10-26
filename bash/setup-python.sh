f17ede846576cb168f2586799dbc1e2c00a7c982
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2025-10-26 20:45:49 +0000

    updated pythong script to run

--- bash/setup-python.sh
+++ bash/setup-python.sh
@@ -6,3 +6,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 python3 -m venv ../python-venv
 source ../python-venv/bin/activate
 pip install -r ../py-requirements.txt
+python3 /home/dominicks/iot-nest-firmware/dist/python/get-dht22-reading.py
