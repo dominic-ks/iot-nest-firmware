0d17618b707cf5955d6e95c213b82734847467a6
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2025-10-26 21:24:06 +0000

    run in project root

--- bash/setup-python.sh
+++ bash/setup-python.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-python3 -m venv ../python-venv
-source ../python-venv/bin/activate
-pip install -r ../py-requirements.txt
-python3 /home/dominicks/iot-nest-firmware/dist/python/get-dht22-reading.py
+python3 -m venv python-venv
+source python-venv/bin/activate
+pip install -r py-requirements.txt
+python3 dist/python/get-dht22-reading.py
