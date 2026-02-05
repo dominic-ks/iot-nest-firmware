c8077bd856e3d881dfd9ec3a9fee5ec06fe6aeec
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2026-02-05 14:33:13 +0000

    remove obsolete scripts

--- bash/docker-build-prod.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker buildx build -f Dockerfile.App  --platform linux/amd64,linux/arm64,linux/arm/v6 -t europe-west2-docker.pkg.dev/iot-projects-309015/iot-nest-firmware/bdvs/iot-nest-firmware:latest --push .
--- bash/setup-buildx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --privileged --rm tonistiigi/binfmt --install all
-docker buildx create --name iot-nest-builder --driver docker-container --bootstrap --use
